ccdad7a061975c7c9c598b869df33baa9b3e8ea2
Hosseinreza <hrakbari77@gmail.com>
2023-04-04 18:06:52 +0330

    Add background img

--- app/src/components/CreateArea.jsx
+++ app/src/components/CreateArea.jsx
@@ -1,10 +1,15 @@
 import React, {useState} from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Zoom from '@mui/material/Zoom';
+
+
 
 function CreateArea(props) {
-   const [ note , setNote] = useState({
-    title:"",
-    content:""
-    })
+    const [ isExpanded, setIsExpanded] = useState(false);
+    const [ note , setNote] = useState({
+        title:"",
+        content:""
+    });
     function handleChange(event){
         const {name, value} = event.target;
         setNote( (prevValues) => {
@@ -12,7 +17,7 @@ function CreateArea(props) {
                 ...prevValues,
                 [name]:value
             }
-        })
+        });
     }
     function submitNote(event){
         props.onAdd(note);
@@ -22,22 +27,34 @@ function CreateArea(props) {
         })
         event.preventDefault();
     }
+    function expand(){
+        setIsExpanded(true);
+    }
   return (
     <div>
       <form>
-        <input 
-            onChange={handleChange} 
-            value={note.title} name="title" 
-            placeholder="Title"  />
+       {isExpanded &&  
+            <input 
+                onChange={handleChange} 
+                value={note.title} name="title" 
+                placeholder="Title"  
+
+            />
+        }
         <textarea 
+            onClick={expand}
             onChange={handleChange} 
             value={note.content} 
             name="content" 
             placeholder="Take a note..." 
-            rows="3"  />
-        <button 
-        onClick={submitNote}
-        >Add</button>
+            rows={isExpanded ? "3" : "1"}  />
+       
+        <Zoom in={isExpanded}>
+            <button onClick={submitNote}>
+                <AddIcon/>
+            </button>
+        </Zoom>
+        
       </form>
     </div>
   );
