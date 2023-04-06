0f5b4a7f4637775a7714f12c60f639cfa6591fcc
Hosseinreza <hrakbari77@gmail.com>
2023-04-06 16:29:18 +0330

    initial for part3

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,28 +1,18 @@
 import React from "react";
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
+import Header from "./Header";
+import Footer from "./Footer";
 import Note from "./Note";
-import { notes } from "../notes.js";
+import CreateArea from "./CreateArea";
 
-function App(){
-    return(
-        <div>
-            <Header/>
-            
-            {notes.map( note => (
-                    <Note 
-                        key={note.key} 
-                        title= { note.title } 
-                        content= { note.content }
-
-                    />
-                )
-            )}
-            <Footer/>
-        </div>
-       
-    );
+function App() {
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
+    </div>
+  );
 }
-
 
 export default App;
