826ef22eab524484ca077d7cdf20b6473e3258cf
Hosseinreza <hrakbari77@gmail.com>
2023-04-06 17:00:05 +0330

    Add note in createArea

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function addNotes(title, content){
+        setNotes( (prevNotes )=>{
+            return ( [...prevNotes, {title, content}] )
+        } );
+        
+
+    }
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNotes}/>
+      {notes.map( (eachNote, index)=>{
+        <Note key={index} title={eachNote.title} content={eachNote.content} />
+      })}
       <Note key={1} title="Note title" content="Note content" />
+
+      
       <Footer />
     </div>
   );
